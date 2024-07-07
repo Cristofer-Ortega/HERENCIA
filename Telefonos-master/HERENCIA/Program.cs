3d3d98613db32d248af3b6957028db5ac21c7e6e
Cristofer-Ortega <143019374+Cristofer-Ortega@users.noreply.github.com>
2024-07-06 19:24:17 -0600

    Add files via upload

---
+++ Telefonos-master/HERENCIA/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HERENCIA
+{
+    public class Telefono
+    {
+        // Propiedades del teléfono
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+
+        // Método para mostrar la información del teléfono
+        public void MostrarInfo()
+        {
+            Console.WriteLine($"Marca {Marca} Modelo: {Modelo}, Precio: ${Precio}, Stock: {Stock}");
+        }
+    }
+
+    public class Program
+    {
+        //inventario
+        public static List<Telefono> Inventario = new List<Telefono>();
+
+        // Método para mostrar el inventario de teléfonos
+        public void MostrarInventario()
+        {
+            Console.WriteLine("Inventario de Teléfonos:");
+            foreach (var telefono in Inventario)
+            {
+                telefono.MostrarInfo();
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program programa = new Program();
+
+            // Bucle principal del programa
+            while (true)
+            {
+                // Menú
+                Console.WriteLine("=========Menu==========");
+                Console.WriteLine("1. Registrar Teléfono");
+                Console.WriteLine("2. Mostrar Inventario");
+                Console.WriteLine("3. Salir");
+                Console.WriteLine("=======================");
+                Console.WriteLine("Seleccione una opción: ");
+
+                string op = Console.ReadLine();
+
+                switch (op)
+                {
+                    case "1":
+                    // Opcion 1: registrar telefonos
+                        RegistrarTelefono registrador = new RegistrarTelefono();
+                        registrador.Registrar(Inventario);
+                        break;
+
+                    // opcion 2: Mostrar el inventario de teléfonos
+                    case "2":
+                        programa.MostrarInventario();
+                        break;
+
+                    // opcion 3: Salir del programa
+                    case "3":
+                        return;
+
+                    default:
+                        Console.WriteLine("Opción no válida, intente nuevamente.");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Telefonos-master/HERENCIA/RegistrarTelefono.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HERENCIA
+{
+    // Clase para registrar un teléfono en el inventario
+    public class RegistrarTelefono
+    {
+        public void Registrar(List<Telefono> Inventario)
+        {
+            Telefono telefono = new Telefono();
+
+           
+            Console.WriteLine("Seleccione La Marca:");
+            Console.WriteLine("1. Samsung");
+            Console.WriteLine("2. Apple");
+            Console.WriteLine("3. Xiaomi");
+            Console.WriteLine("4. Huawei");
+            Console.WriteLine("5. OnePlus");
+
+            string opcionMarca;
+            do
+            {
+                opcionMarca = Console.ReadLine();
+                switch (opcionMarca)
+                {
+                    case "1":
+                        telefono.Marca = "Samsung";
+                        break;
+                    case "2":
+                        telefono.Marca = "Apple";
+                        break;
+                    case "3":
+                        telefono.Marca = "Xiaomi";
+                        break;
+                    case "4":
+                        telefono.Marca = "Huawei";
+                        break;
+                    case "5":
+                        telefono.Marca = "OnePlus";
+                        break;
+                    default:
+                        // Mostrar mensaje de error si la opción de marca es inválida
+                        Console.WriteLine("Opción inválida.");
+                        break;
+                }
+            } while (telefono.Marca == null); 
+
+            Console.Write("Ingrese el modelo: ");
+            telefono.Modelo = Console.ReadLine();
+
+            Console.Write("Ingrese el precio: ");
+            telefono.Precio = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el stock: ");
+            telefono.Stock = int.Parse(Console.ReadLine());
+
+            Inventario.Add(telefono);
+
+            Console.WriteLine("Teléfono registrado exitosamente.");
+        }
+    }
+}
